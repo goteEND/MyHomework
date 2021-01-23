608cc59e88d92904af16de5aec59463519c1cbdf
goteEND <sebi.marina20@gmail.com>
2021-01-23 19:20:29 +0200

    Added Entities

---
+++ MyHomework.API/MyHomework.API/Entities/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyHomework.API.Entities
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string EnrolledStudent { get; set; }
+        public string GithubLink { get; set; }
+        public string DueDate { get; set; } 
+    }
+}
---
+++ MyHomework.API/MyHomework.API/Entities/Subject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyHomework.API.Entities
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Project> Projects { get; set; }  
+    }
+}
+    
